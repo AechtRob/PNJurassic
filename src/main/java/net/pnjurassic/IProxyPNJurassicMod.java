9039eb433dd415e609842f592884982300dfe5ad
AechtRob <aecht_rob@outlook.com>
2022-09-29 23:59:51 +0100

    Initial commit

---
+++ src/main/java/net/pnjurassic/IProxyPNJurassicMod.java
@@ -0,0 +1,17 @@
+package net.pnjurassic;
+
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+public interface IProxyPNJurassicMod {
+    void preInit(FMLPreInitializationEvent event);
+
+    void init(FMLInitializationEvent event);
+
+    void postInit(FMLPostInitializationEvent event);
+
+    void serverLoad(FMLServerStartingEvent event);
+}
